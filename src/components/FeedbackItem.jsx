7a03736034b7497e601c826270ef260e2dbbaec2
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-25 22:37:38 -1000

    Commit feedback item component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -5,7 +5,9 @@ function FeedbackItem({ item }) {
     return (
         <Card>
             <div className="num-display">{item.rating}</div>
-            <button className="close"><FaTimes color='purple'/></button>
+            <button className="close">
+                <FaTimes color='purple'/>
+            </button>
             <div className="text-display">{item.text}
             </div>
         </Card>
