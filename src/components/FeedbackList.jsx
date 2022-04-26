a7a2c383e1ac0cb249c853ca337a461b0ccf66df
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-25 22:22:26 -1000

    Creates feedback list component, embeds feedback item component

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,18 @@
+import FeedbackItem from './FeedbackItem'
+
+function FeedbackList({ feedback }) {
+
+    if(!feedback || feedback.length === 0) {
+        return <p>No reviews yet...</p>
+    }
+
+    return (
+    <div className='feedback-list'>
+        {feedback.map((item) => 
+            <FeedbackItem key={item.id} item={item} />
+        )}
+    </div>
+  )
+}
+
+export default FeedbackList
