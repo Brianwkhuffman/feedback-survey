2fbfcb725836f66e3a1ec2035af003da04398a49
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-25 22:31:05 -1000

    Creates card component to use for feedback items

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,12 @@
+import Card from './shared/Card'
+
 function FeedbackItem({ item }) {
     return (
-        <div className='card'>
+        <Card>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}
             </div>
-        </div>
+        </Card>
     )
 }
 
---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,7 @@
+function Card({ children }) {
+  return (
+    <div className='card'>{children}</div>
+  )
+}
+
+export default Card
