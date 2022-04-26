b70dfaf9ebb74c7075800de41039ee2647e28de0
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-25 22:36:56 -1000

    Install icon package for delete icon, import icon into feedback itm component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,11 @@
+import { FaTimes } from 'react-icons/fa'
 import Card from './shared/Card'
 
 function FeedbackItem({ item }) {
     return (
         <Card>
             <div className="num-display">{item.rating}</div>
+            <button className="close"><FaTimes color='purple'/></button>
             <div className="text-display">{item.text}
             </div>
         </Card>
