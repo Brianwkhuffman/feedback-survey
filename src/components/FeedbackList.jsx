774dbbd518c5968f589c71d2adbcee4c4e738525
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-26 22:13:36 -1000

    Installs frame-motion package, imports to feedback list component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList({ feedback, handleDelete }) {
@@ -8,13 +9,22 @@ function FeedbackList({ feedback, handleDelete }) {
 
     return (
     <div className='feedback-list'>
-        {feedback.map((item) => 
-            <FeedbackItem 
-            key={item.id}
-            item={item}
-            handleDelete={handleDelete}
-            />
-        )}
+        <AnimatePresence>
+            {feedback.map((item) => 
+                <motion.div
+                    key={item.id}
+                    intial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    transition={{duration: 1}}>
+
+                    <FeedbackItem 
+                    key={item.id}
+                    item={item}
+                    handleDelete={handleDelete}/>
+                </motion.div>
+            )}
+        </AnimatePresence>
     </div>
   )
 }
