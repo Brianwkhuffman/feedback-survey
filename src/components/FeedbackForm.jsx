8bcae12aef3413cd193476794ba1fb1b5bf1acce
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-26 19:50:23 -1000

    Adds validation on review input field, checks for lenght

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,23 +4,39 @@ import Button from './shared/Button'
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [validationMsg, setValidationMessage] = useState('')
 
     const handleTextChange = (e) => {
-        console.log(e.target.value)
+        if (text === '') {
+            setBtnDisabled(true)
+            setValidationMessage(null)
+        } else if (text.trim().length < 10) {
+            setBtnDisabled(true)
+            setValidationMessage('Review must contain at least 10 characters...')
+        } else {
+            setValidationMessage(null)
+            setBtnDisabled(false)
+        }
+        setText(e.target.value)
     }
 
     return (
       <Card>
           <form>
               <h2>Did you enjoy our service?</h2>
+              {/* need rating select component */}
               <div className="input-group">
                   <input 
                   type='text'
                    placeholder='Write a review'
-                   onChange={handleTextChange}
-                   />
-                  <Button type='submit' version='secondary'>Submit</Button>
+                   onChange={handleTextChange}/>
+                  <Button 
+                  type='submit'
+                  version='secondary'
+                  isDisabled={btnDisabled}>Submit</Button>
               </div>
+              {validationMsg && <div className='message'>{validationMsg}</div>}
           </form>
       </Card>
     )
