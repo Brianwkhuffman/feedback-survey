64a1b1c64b04fca550b8c4c695454f9bcb18dc3d
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-26 18:41:54 -1000

    Adds a state component, displays num of reviews and avg review rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
 
 function App() {
 
@@ -18,6 +19,8 @@ function App() {
         <>
             <Header/>
             <div className='container'>
+                <FeedbackStats feedback={feedback} />
+                
                 <FeedbackList 
                 feedback={feedback}
                 handleDelete={deleteFeedback}
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,15 @@
+function FeedbackStats({ feedback }) {
+    let averageRating = feedback.reduce((acc, curr) => {
+        return acc + curr.rating;
+    }, 0) / feedback.length;
+    averageRating = averageRating.toFixed(1).replace(/[,.]0$/, '')
+
+    return (
+      <div className="feedback-stats">
+          <h4>{feedback.length} Reviews</h4>
+          <h4>Average Rating: {isNaN(averageRating) ? 0 : averageRating}</h4>
+      </div>
+  )
+}
+
+export default FeedbackStats
