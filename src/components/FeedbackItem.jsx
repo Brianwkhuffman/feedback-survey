ac6e2c8683260f4c5f6ef0473fb4b89a388d50bf
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-28 22:38:59 -1000

    Refactors feedback item to use context for delete

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,15 @@
 import { FaTimes } from 'react-icons/fa'
 import Card from './shared/Card'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem({ item, handleDelete }) {
+function FeedbackItem({ item }) {
+    const {deleteFeedback} = useContext(FeedbackContext)
+
     return (
         <Card>
             <div className="num-display">{item.rating}</div>
-            <button onClick={() => handleDelete(item.id)} className="close">
+            <button onClick={() => deleteFeedback(item.id)} className="close">
                 <FaTimes color='purple'/>
             </button>
             <div className="text-display">{item.text}
