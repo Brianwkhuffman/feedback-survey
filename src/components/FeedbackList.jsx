4085f68ea124b45132990e03dd4289598db97579
Brianwkhuffman <brianwkhuffman@gmail.com>
2022-04-28 22:37:39 -1000

    Removes delete prop from feedback list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({ handleDelete }) {
+function FeedbackList() {
     const {feedback} = useContext(FeedbackContext)
 
     if(!feedback || feedback.length === 0) {
@@ -23,8 +23,7 @@ function FeedbackList({ handleDelete }) {
 
                     <FeedbackItem 
                     key={item.id}
-                    item={item}
-                    handleDelete={handleDelete}/>
+                    item={item} />
                 </motion.div>
             )}
         </AnimatePresence>
